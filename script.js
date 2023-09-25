6becc503bb1b1f41735bfab58f9292db4d6a5f3b
cliffmeister <dev.clfm5t12@gmail.com>
2023-09-25 20:54:51 +0800

    Separated addresspoints into a separate JSON file

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-
-var addressPoints = [
-  {
-   lat:8.152254, 
-   long:124.784990,
-   title:"Mt. Kitanglad Mountain Range"
-  },
-
-  {
-   lat:7.961891, 
-   long:124.741015,
-   title:"Mt. Kalatungan Mountain Range"
-  },
-  {
-   lat:7.961891, 
-   long:124.741015,
-   title:"Mt. Kalatungan Mountain Range"
-  }
-
-];
-
-
 var map = L.map('map').setView([8.011424, 125.279184], 5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,28 +6,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markers = L.markerClusterGroup();
 
-//Markers
-for (var i = 0; i < addressPoints.length; i++)
-{
- // var a = addressPoints[i];
-  var title = addressPoints[i].title;
-  var marker = L.marker(new L.LatLng(addressPoints[i].lat, addressPoints[i].long),
-  {});
+// Load the JSON data from a file
+fetch('pmr.json') // Replace 'addressPoints.json' with the actual path to your JSON file
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(point => {
+      var title = point.title;
+      var marker = L.marker(new L.LatLng(point.lat, point.long), {});
 
-  
-  marker.bindPopup( " <h2>"+title+"</h2>" );
+      marker.bindPopup("<h2>" + title + "</h2>");
 
+      markers.addLayer(marker);
 
-  markers.addLayer(marker);
-  
-  marker.on('click', function(ev){
-    var latlng = map.mouseEventToLatLng(ev.originalEvent);
-    console.log(latlng.lat + ', ' + latlng.lng);
+      marker.on('click', function (ev) {
+        var latlng = map.mouseEventToLatLng(ev.originalEvent);
+        console.log(latlng.lat + ', ' + latlng.lng);
+      });
+    });
+
+    map.addLayer(markers);
+  })
+  .catch(error => {
+    console.error('Error loading JSON:', error);
   });
-  
-
-}
-
-
-
-map.addLayer(markers);
